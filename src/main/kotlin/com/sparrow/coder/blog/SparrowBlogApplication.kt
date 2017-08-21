ab1dd98ea264cf50b47311e41d40d99242b73162
spacoder <sparrow.coder@gmail.com>
2017-08-21 23:16:19 +0800

    Set up basic Spring Boot framework

---
+++ src/main/kotlin/com/sparrow/coder/blog/SparrowBlogApplication.kt
@@ -0,0 +1,11 @@
+package com.sparrow.coder.blog
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SparrowBlogApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SparrowBlogApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/sparrow/coder/blog/SparrowBlogApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.sparrow.coder.blog
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SparrowBlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
